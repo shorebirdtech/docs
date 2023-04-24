0b99715d24e95058d74f52d526119942d6be870f
Felix Angelov <felix@shorebird.dev>
2023-04-24 14:12:38 -0500

    tweak

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,6 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'shorebirdtech',
   projectName: 'docs',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],

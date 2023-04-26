1d8608e5803f5468628ba1075beaf7d088f9749e
Felix Angelov <felix@shorebird.dev>
2023-04-26 11:38:02 -0500

    chore: fix favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.svg',
   organizationName: 'shorebirdtech',
   trailingSlash: false,
   projectName: 'docs',

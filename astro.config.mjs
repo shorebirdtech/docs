51cf7ee3d6bbdf2bc9fa14c31e0db4301b69e878
Eric Seidel <eric@shorebird.dev>
2024-11-13 14:22:52 -0800

    docs: Add a page on billing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -133,6 +133,10 @@ export default defineConfig({
           link: '/status',
         },
         {
+          label: 'Billing',
+          link: '/billing',
+        },
+        {
           label: 'Architecture',
           link: '/architecture',
         },
